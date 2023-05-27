5095d9c8fcb1d5deadc5d6b4214d77bf6332f263
gustavopessoal <devgustavomota@gmail.com>
2023-05-26 22:12:48 -0300

    Remove comments

--- functions/db.js
+++ functions/db.js
@@ -1,15 +1,8 @@
 const mysql = require("mysql");
 
 exports.db = mysql.createConnection({
-  host: "us-cdbr-east-06.cleardb.net",
-  user: "ba3361d9963c59",
-  password: "78504133",
-  database: "heroku_e9da8e1bcac571c",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
-
-// exports.db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "@Mota2018",
-//   database: "chatgpt",
-// });
