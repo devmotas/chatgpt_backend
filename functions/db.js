7695de6a295af4a12a2008424fe28d435fac6746
gustavopessoal <devgustavomota@gmail.com>
2023-05-27 13:42:55 -0300

    Add erros to reconect

--- functions/db.js
+++ functions/db.js
@@ -1,10 +1,12 @@
+/* eslint-disable max-len */
 /* eslint-disable require-jsdoc */
 require("dotenv").config();
 
-
 const mysql = require("mysql");
 
+
 let db;
+let retryCount = 0;
 
 function handleDisconnect() {
   db = mysql.createConnection({
@@ -17,20 +19,32 @@ function handleDisconnect() {
   db.connect((err) => {
     if (err) {
       console.error("Erro ao conectar ao banco de dados:", err);
-      setTimeout(handleDisconnect, 2000); // Tentar reconectar após 2 segundos
+      if (retryCount < 10) {
+        retryCount++;
+        console.log("Tentando reconectar. Tentativa", retryCount);
+        setTimeout(handleDisconnect, 2000); // Tentar reconectar após 2 segundos
+      } else {
+        console.error("Número máximo de tentativas excedido. Não é possível conectar ao banco de dados.");
+      }
+    } else {
+      console.log("Conexão bem-sucedida!");
+      retryCount = 0; // Zerar o contador em caso de sucesso
     }
   });
 
   db.on("error", (err) => {
     console.error("Erro no banco de dados:", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST" ||
-     err.code === "PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR") {
+    if (retryCount < 10) {
+      retryCount++;
+      console.log("Tentando reconectar. Tentativa", retryCount);
       handleDisconnect(); // Reconectar se a conexão for perdida
     } else {
+      console.error("Número máximo de tentativas excedido. Não é possível reconectar ao banco de dados.");
       throw err;
     }
   });
 }
+
 
 handleDisconnect();
 
