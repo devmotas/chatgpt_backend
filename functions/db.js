09efb5a4c19aca547203d9c54d62683323642235
gustavopessoal <devgustavomota@gmail.com>
2023-05-27 12:50:37 -0300

    testing new conection

--- functions/db.js
+++ functions/db.js
@@ -1,12 +1,40 @@
+/* eslint-disable require-jsdoc */
+
 require("dotenv").config();
 
+const mysql = require("mysql2");
 
-const mysql = require("mysql");
-
-
-exports.db = mysql.createConnection({
+const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-});
+};
+
+let db;
+
+function handleDisconnect() {
+  db = mysql.createConnection(dbConfig);
+
+  db.connect((err) => {
+    if (err) {
+      console.error("Erro ao conectar ao banco de dados:", err);
+      setTimeout(handleDisconnect, 2000); // Tentar reconectar após 2 segundos
+    } else {
+      console.log("Conexão estabelecida com sucesso!");
+    }
+  });
+
+  db.on("error", (err) => {
+    console.error("Erro na conexão com o banco de dados:", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleDisconnect(); // Tentar reconectar
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+
+exports.db = db.promise();
